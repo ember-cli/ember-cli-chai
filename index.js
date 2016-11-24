d88a3e2221bfd1db496aee7a9bcf92a4cc6290ef
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-24 18:34:56 +0100

    Import "chai" from NPM

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var resolve = require('resolve');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
   name: 'ember-cli-chai',
-  included: function (app) {
-    this._super.included(app);
 
-    app.import(app.bowerDirectory + '/chai/chai.js');
-    app.import('vendor/ember-cli-chai/ember-cli-chai.js');
+  included: function included(app) {
+    this._super.included.apply(this, arguments);
 
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    app.import('vendor/chai/chai.js', {
+      type: 'test'
+    });
+  },
+
+  treeForVendor: function(/* tree */) {
+    var chaiPath = path.dirname(resolve.sync('chai'));
+    return new Funnel(chaiPath, {
+      files: ['chai.js'],
+      destDir: '/chai',
+    });
   }
 };
