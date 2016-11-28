10480e8599d11297063da7817ff36b2b6e7191a6
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-28 21:24:19 +0100

    Cleanup plugin import code

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ var Funnel = require('broccoli-funnel');
 var MergeTrees = require('broccoli-merge-trees');
 var VersionChecker = require('ember-cli-version-checker');
 
+var jqueryPlugin = {
+  name: 'chai-jquery',
+  constraint: '^2.0.0',
+  path: 'chai-jquery.js',
+};
+
+var domPlugin = {
+  name: 'chai-dom',
+  constraint: '^1.0.0',
+  path: 'chai-dom.js',
+};
+
+var supportedPlugins = [jqueryPlugin, domPlugin];
+
 module.exports = {
   name: 'ember-cli-chai',
 
@@ -17,13 +31,18 @@ module.exports = {
     var devDependencies = Object.keys(this.project.pkg.devDependencies || {});
     var checker = new VersionChecker(this);
 
-    this.importChaiJQuery =
-      (dependencies.indexOf('chai-jquery') !== -1 || devDependencies.indexOf('chai-jquery') !== -1) &&
-      checker.for('chai-jquery', 'npm').satisfies('^2.0.0');
+    this.plugins = supportedPlugins.filter(function(plugin) {
+      return (dependencies.indexOf(plugin.name) !== -1 || devDependencies.indexOf(plugin.name) !== -1) &&
+        checker.for(plugin.name, 'npm').satisfies(plugin.constraint);
+    });
 
-    this.importChaiDOM =
-      (dependencies.indexOf('chai-dom') !== -1 || devDependencies.indexOf('chai-dom') !== -1) &&
-      checker.for('chai-dom', 'npm').satisfies('^1.0.0');
+    // filter out `chai-dom` if `chai-jquery` is also installed as
+    // having both plugins active results in conflicts
+    let domPluginIndex = this.plugins.indexOf(domPlugin);
+    let jqueryPluginIndex = this.plugins.indexOf(jqueryPlugin);
+    if (jqueryPluginIndex !== -1 && domPluginIndex !== -1) {
+      this.plugins.splice(domPluginIndex, 1);
+    }
   },
 
   included: function included(app) {
@@ -36,12 +55,9 @@ module.exports = {
     app.import('vendor/chai/chai.js', { type: 'test' });
     app.import('vendor/shims/chai.js', { type: 'test' });
 
-    if (this.importChaiJQuery) {
-      app.import('vendor/chai/chai-jquery.js', { type: 'test' });
-
-    } else if (this.importChaiDOM) {
-      app.import('vendor/chai/chai-dom.js', { type: 'test' });
-    }
+    this.plugins.forEach(function(plugin) {
+      app.import('vendor/chai/' + plugin.path, { type: 'test' });
+    });
   },
 
   treeForAddon: function() {
@@ -59,24 +75,16 @@ module.exports = {
 
     var trees = [tree, chaiTree];
 
-    if (this.importChaiJQuery) {
-      var chaiJQueryPath = path.dirname(resolve.sync('chai-jquery', { basedir: this.project.root }));
-      var chaiJQueryTree = new Funnel(chaiJQueryPath, {
-        files: ['chai-jquery.js'],
+    let basedir = this.project.root;
+    this.plugins.forEach(function(plugin) {
+      var pluginPath = path.dirname(resolve.sync(plugin.name, { basedir: basedir }));
+      var pluginTree = new Funnel(pluginPath, {
+        files: [plugin.path],
         destDir: '/chai',
       });
 
-      trees.push(chaiJQueryTree);
-
-    } else if (this.importChaiDOM) {
-      var chaiDOMPath = path.dirname(resolve.sync('chai-dom', { basedir: this.project.root }));
-      var chaiDOMTree = new Funnel(chaiDOMPath, {
-        files: ['chai-dom.js'],
-        destDir: '/chai',
-      });
-
-      trees.push(chaiDOMTree);
-    }
+      trees.push(pluginTree);
+    });
 
     return new MergeTrees(trees, {
       annotation: 'ember-cli-chai: treeForVendor'
