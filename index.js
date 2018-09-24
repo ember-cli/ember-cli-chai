804eca13170d91b498ce741234b31d7bbd3a79dd
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-09-24 08:16:49 +0200

    Use ES6 features

--- index.js
+++ index.js
@@ -2,35 +2,35 @@
 
 /* eslint-env node */
 
-var path = require('path');
-var resolve = require('resolve');
-var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
-var VersionChecker = require('ember-cli-version-checker');
-var Rollup = require('broccoli-rollup');
-var commonjs = require('rollup-plugin-commonjs');
-var nodeResolve = require('rollup-plugin-node-resolve');
-var babel = require('rollup-plugin-babel');
+const path = require('path');
+const resolve = require('resolve');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const VersionChecker = require('ember-cli-version-checker');
+const Rollup = require('broccoli-rollup');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
 
-var jqueryPlugin = {
+const jqueryPlugin = {
   name: 'chai-jquery',
   constraint: '^2.0.0',
   path: 'chai-jquery.js',
 };
 
-var domPlugin = {
+const domPlugin = {
   name: 'chai-dom',
   constraint: '^1.0.0',
   path: 'chai-dom.js',
 };
 
-var asPromisedPlugin = {
+const asPromisedPlugin = {
   name: 'chai-as-promised',
   constraint: '<6',
   path: 'chai-as-promised.js',
 };
 
-var asPromisedPlugin6 = {
+const asPromisedPlugin6 = {
   name: 'chai-as-promised',
   constraint: '^6 || ^7',
   path: 'chai-as-promised.js',
@@ -50,20 +50,20 @@ var asPromisedPlugin6 = {
   },
 };
 
-var sinonPlugin = {
+const sinonPlugin = {
   name: 'sinon-chai',
   constraint: '>=2.0.0',
   path: 'sinon-chai.js',
 };
 
-var testdoublePlugin = {
+const testdoublePlugin = {
   name: 'testdouble-chai',
   constraint: '^0.5.0',
   path: 'testdouble-chai.js',
   supportFile: 'testdouble-chai.js'
 };
 
-var supportedPlugins = [
+const supportedPlugins = [
   jqueryPlugin,
   domPlugin,
   asPromisedPlugin,
@@ -75,36 +75,36 @@ var supportedPlugins = [
 module.exports = {
   name: 'ember-cli-chai',
 
-  init: function() {
+  init() {
     this._super.init && this._super.init.apply(this, arguments);
 
-    var dependencies = Object.keys(this.project.pkg.dependencies || {});
-    var devDependencies = Object.keys(this.project.pkg.devDependencies || {});
-    var checker = new VersionChecker(this);
+    let dependencies = Object.keys(this.project.pkg.dependencies || {});
+    let devDependencies = Object.keys(this.project.pkg.devDependencies || {});
+    let checker = new VersionChecker(this);
 
-    this.plugins = supportedPlugins.filter(function(plugin) {
+    this.plugins = supportedPlugins.filter(plugin => {
       return (dependencies.indexOf(plugin.name) !== -1 || devDependencies.indexOf(plugin.name) !== -1) &&
         checker.for(plugin.name, 'npm').satisfies(plugin.constraint);
     });
 
     // filter out `chai-dom` if `chai-jquery` is also installed as
     // having both plugins active results in conflicts
-    var domPluginIndex = this.plugins.indexOf(domPlugin);
-    var jqueryPluginIndex = this.plugins.indexOf(jqueryPlugin);
+    let domPluginIndex = this.plugins.indexOf(domPlugin);
+    let jqueryPluginIndex = this.plugins.indexOf(jqueryPlugin);
     if (jqueryPluginIndex !== -1 && domPluginIndex !== -1) {
       this.plugins.splice(domPluginIndex, 1);
     }
 
     // ensure that `sinon-chai` and `testdouble-chai` aren't both enabled,
     // since they use the same API
-    var sinonIndex = this.plugins.indexOf(sinonPlugin);
-    var tdIndex = this.plugins.indexOf(testdoublePlugin);
+    let sinonIndex = this.plugins.indexOf(sinonPlugin);
+    let tdIndex = this.plugins.indexOf(testdoublePlugin);
     if (sinonIndex !== -1 && tdIndex !== -1) {
       this.plugins.splice(tdIndex, 1);
     }
   },
 
-  included: function included(app) {
+  included(app) {
     this._super.included.apply(this, arguments);
 
     while (typeof app.import !== 'function' && app.app) {
@@ -114,39 +114,39 @@ module.exports = {
     app.import('vendor/chai/chai.js', { type: 'test' });
     app.import('vendor/shims/chai.js', { type: 'test' });
 
-    this.plugins.forEach(function(plugin) {
+    for (let plugin of this.plugins) {
       app.import('vendor/chai/' + plugin.path, { type: 'test' });
 
       if (plugin.supportFile) {
         app.import('vendor/chai-plugin-support/' + plugin.supportFile, { type: 'test' });
       }
-    });
+    }
   },
 
-  treeForAddon: function() {
+  treeForAddon() {
     if (this.app.tests) {
       return this._super.treeForAddon.apply(this, arguments);
     }
   },
 
-  treeForVendor: function(tree) {
-    var chaiPath = path.dirname(resolve.sync('chai'));
-    var chaiTree = new Funnel(chaiPath, {
+  treeForVendor(tree) {
+    let chaiPath = path.dirname(resolve.sync('chai'));
+    let chaiTree = new Funnel(chaiPath, {
       files: ['chai.js'],
       destDir: '/chai',
     });
 
-    var trees = [tree, chaiTree];
+    let trees = [tree, chaiTree];
 
-    var basedir = this.project.root;
-    this.plugins.forEach(function(plugin) {
-      var pluginTree;
+    let basedir = this.project.root;
+    for (let plugin of this.plugins) {
+      let pluginTree;
 
       if (plugin.rollup) {
         pluginTree = new Rollup(__dirname + '/rollup', { rollup: plugin.rollup });
 
       } else {
-        var pluginPath = path.dirname(resolve.sync(plugin.name, { basedir: basedir }));
+        let pluginPath = path.dirname(resolve.sync(plugin.name, { basedir: basedir }));
         pluginTree = new Funnel(pluginPath, {
           files: [plugin.path],
           destDir: '/chai',
@@ -154,7 +154,7 @@ module.exports = {
       }
 
       trees.push(pluginTree);
-    });
+    }
 
     return new MergeTrees(trees, {
       annotation: 'ember-cli-chai: treeForVendor'
